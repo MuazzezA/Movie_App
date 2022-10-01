bed7c4e0199fa1bb4a9e77a1094cf5fe0652a717
MuazzazA <ayd.muazzez@gmail.com>
2022-10-02 01:43:01 +0300

    moved App.js

---
+++ Movie_App/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigations from './navigation/Navigations';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+export default function App() {
+  
+  return (
+    <SafeAreaProvider>
+      <Navigations/>
+    </SafeAreaProvider>
+  );
+}
+
