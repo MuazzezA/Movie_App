b050ab5e7ca2ea3608d4ed883e990b4346e8f8b8
MuazzazA <ayd.muazzez@gmail.com>
2022-10-03 00:23:01 +0300

    added tabbar to navigations

--- Movie_App/src/navigation/Navigations.js
+++ Movie_App/src/navigation/Navigations.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import HomeScreen from '../screens/HomeScreen';
+import HomeStackScreen from '../screens/HomeStackScreen';
+import SettingsStackScreen from '../screens/SettingsStackScreen';
 
-const Stack = createNativeStackNavigator();
+
+const Tab = createBottomTabNavigator();
 
 export default function Navigations(){
 
     return(
         
+        // <NavigationContainer>
+        //     <Stack.Navigator initialRouteName = "Home"
+        //     screenOptions={{
+        //         contentStyle:{
+        //           backgroundColor:'#F0EBE3'
+        //         }
+        //      }}
+        //     >
+        //         <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+        //     </Stack.Navigator>
+        // </NavigationContainer>
         <NavigationContainer>
-            <Stack.Navigator initialRouteName = "Home">
-                <Stack.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
-            </Stack.Navigator>
-        </NavigationContainer>
+        <Tab.Navigator
         
+        initialRouteName = "Home"
+            // screenOptions={{
+            //     contentStyle:{
+            //       backgroundColor:'#F0EBE3',
+                  
+            //     }
+                
+            //  }}
+             activeColor="#f0edf6"
+             inactiveColor="#3e2465"
+             barStyle={{ backgroundColor: '#694fad' }}
+        >
+            <Tab.Screen name="HomeStackScreen" component={HomeStackScreen} options={{headerShown:false}}/>
+            <Tab.Screen name="SettingsStackScreen" component={SettingsStackScreen} options={{headerShown:false}}/>
+        </Tab.Navigator>
+        </NavigationContainer>
     );
 
 
