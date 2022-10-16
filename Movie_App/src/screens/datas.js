ba88b80a2a494a2ef66d8e89712cdee3d01d6187
MuazzazA <ayd.muazzez@gmail.com>
2022-10-17 01:15:07 +0300

    moved noSearch and noData

---
+++ Movie_App/src/screens/datas.js
@@ -0,0 +1,25 @@
+export const noData = [
+  {
+    id: "1",
+    i: {
+      height: 1500,
+      imageUrl: "./../assets/No_data-cuate.png",
+      width: 1013,
+    },
+    l: "I dont Found Data For Search",
+    s: "",
+  },
+];
+
+export const noSearch = [
+  {
+    id: "1",
+    i: {
+      height: 1500,
+      imageUrl: "./../assets/No_data-cuate.png",
+      width: 1013,
+    },
+    l: "Please Search Movie",
+    s: "",
+  },
+];
