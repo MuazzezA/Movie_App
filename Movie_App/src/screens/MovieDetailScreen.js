a6dc92b8495bcfb01b0e33be5519fcc1d09a1b2a
MuazzazA <ayd.muazzez@gmail.com>
2022-10-17 01:16:55 +0300

    movie detail screen

--- Movie_App/src/screens/MovieDetailScreen.js
+++ Movie_App/src/screens/MovieDetailScreen.js
@@ -1,16 +1,68 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Icon from "react-native-vector-icons/Feather";
 
-export default function MovieDetailScreen() {
+export default function MovieDetailScreen({ route }) {
+  const { movie } = route.params;
+
   return (
     <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "space-between",
-        alignItems: "center",
       }}>
-      <View style={{ flex: 1 }}></View>
+      <View style={{ flex: 1 }}>
+        <View style={styles.header}>
+          <View style={{ flexDirection: "column", flex: 1 }}>
+            <Text style={styles.title}>{movie.l}</Text>
+            <Text style={styles.subtitle}>{movie.s}</Text>
+          </View>
+          <Icon name="heart" size={22} style={{ padding: 5 }} />
+        </View>
+
+        <Image
+          style={styles.poster}
+          source={{
+            uri: movie.hasOwnProperty("i")
+              ? movie.i.imageUrl
+              : "https://vectorified.com/images/no-data-icon-10.png",
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: 350,
+    width: 300,
+    borderWidth: 1,
+    padding: 10,
+    marginVertical: 15,
+    borderRadius: 20,
+    borderColor: "#576F72",
+    backgroundColor: "#E4DCCF",
+  },
+  poster: {
+    alignItems: "center",
+    height: 320,
+    width: "100%",
+    resizeMode: "contain",
+    padding: 10,
+    backgroundColor: "black",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    height: 75,
+    width: "100%",
+    backgroundColor: "#b4fCCF",
+    padding: 10,
+  },
+});
