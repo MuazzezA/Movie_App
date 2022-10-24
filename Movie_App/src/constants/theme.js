beaa7144eff5b4c7eeac68222933e6d983fe21a2
MuazzazA <ayd.muazzez@gmail.com>
2022-10-25 00:27:51 +0300

    created constants theme and index

---
+++ Movie_App/src/constants/theme.js
@@ -0,0 +1,19 @@
+export const FONTS = {
+  h1: { fontSize: 30, lineHeight: 36 },
+  h2: { fontSize: 22, lineHeight: 30 },
+  h3: { fontSize: 16, lineHeight: 22 },
+  h4: { fontSize: 14, lineHeight: 22 },
+  body1: {
+    fontSize: 30,
+    lineHeight: 36,
+  },
+
+  body3: {
+    fontSize: 16,
+    lineHeight: 22,
+  },
+};
+
+const appTheme = { FONTS };
+
+export default appTheme;
