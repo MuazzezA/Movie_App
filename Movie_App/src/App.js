2c81ee4add500f25b6fd475627d01c1917e77d85
MuazzazA <ayd.muazzez@gmail.com>
2022-10-25 00:26:46 +0300

    fix navigations in app.js

--- Movie_App/src/App.js
+++ Movie_App/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Navigations from "./navigation/Navigations";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import OnBoardingStackScreen from "./screens/OnBoardingStackScreen";
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <Navigations />
+      <OnBoardingStackScreen />
     </SafeAreaProvider>
   );
 }
